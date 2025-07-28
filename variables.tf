e518dd9e7521d1701a6a81d3c8a658d5a6e12ee1
Rohana Upadhyay <workrohanu23@gmail.com>
2025-07-28 14:57:50 +0000

    Final Fix 3

--- variables.tf
+++ variables.tf
@@ -1,19 +1,7 @@
-variable "aws_access_key" {
-  description = "AWS Access Key"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-  type        = string
-  sensitive   = true
-}
-
 variable "aws_region" {
   description = "AWS Region"
   type        = string
-  default     = "ap-south-1"
+  default     = "us-east-2"
 }
 
 variable "image_uri" {
